192cd71d6649cb65d978140a7cadb5de77e91535
Lishuangqing <shuangqing@TIANYUANdeMacBook-Pro.local>
2016-09-29 19:14:00 +0800

    some

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929091027) do
+ActiveRecord::Schema.define(version: 20160929093842) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.string   "topic"
+    t.integer  "_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
